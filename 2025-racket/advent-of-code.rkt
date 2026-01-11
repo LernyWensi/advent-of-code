2606c47ac4dfb9fbe88d65ecf452eb698d44de44
Lerny <thelerny@gmail.com>
2026-01-11 12:37:28 +0500

    Improve `check` & turn `solve` into a macro

--- 2025-racket/advent-of-code.rkt
+++ 2025-racket/advent-of-code.rkt
@@ -4,20 +4,30 @@
 
 (provide solve check)
 
-(define (solve parse part-1 part-2)
-    (command-line
-        #:args (filename)
-        (define input (string-trim (file->string filename)))
-        (define parsed (parse input))
-        (printf "Part #1: ~a~n" (or (part-1 parsed) "Not Solved"))
-        (printf "Part #2: ~a~n" (or (part-2 parsed) "Not Solved"))))
+(define-syntax (solve stx)
+    (syntax-parse stx
+                  [(_)
+                   #:with parse (datum->syntax stx 'parse)
+                   #:with part-1 (datum->syntax stx 'part-1)
+                   #:with part-2 (datum->syntax stx 'part-2)
+                   #'(command-line
+                       #:args (filename)
+                       (define input (string-trim (file->string filename)))
+                       (define parsed (parse input))
+                       (printf "Part #1: ~a~n" (or (part-1 parsed) "Not Solved"))
+                       (printf "Part #2: ~a~n" (or (part-2 parsed) "Not Solved")))]))
 
 (define-syntax (check stx)
     (syntax-parse stx
-                  [(_ parser solver expected input)
+                  [(_ solver expected/input ...)
                    #:with line (datum->syntax stx (syntax-line stx))
-                   #'(let ()
-                       (define actual (solver (parser input)))
-                       (if (equal? actual expected)
-                           (printf "[~a:~a] [X] ~a == ~a~n" 'solver line expected actual)
-                           (printf "[~a:~a] [_] ~a != ~a~n" 'solver line expected actual)))]))
+                   #:with parse (datum->syntax stx 'parse)
+                   #'(begin
+                       (printf "[~a:~a]~n" 'solver line)
+                       (let ()
+                           (match-define (list expected input) expected/input)
+                           (define actual (solver (parse input)))
+                           (if (equal? actual expected)
+                                (printf "    \033[32m✔ ~a == ~a~n\033[0m" expected actual)
+                                (printf "    \033[31m⨯ ~a != ~a~n\033[0m" expected actual))
+                           ) ... )]))
